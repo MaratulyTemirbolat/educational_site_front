acc40b6a949b8fea179397796fda65a9ad53bd02
Temirbolat <snake2009003@mail.ru>
2023-06-22 16:11:37 +0600

    Updated ts typing, added version to storage and method for fetching user.

--- store/user.store.ts
+++ store/user.store.ts
@@ -1,29 +1,48 @@
 import { create } from "zustand";
-import { persist, createJSONStorage } from 'zustand/middleware'
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 import { User } from "../models/auths.models";
+import { getUserByToken } from "@/services/auth.services";
 
 export type UseUser = {
     user: User | null,
     loading: boolean,
     error: Error | null,
     setUser: (user: User) => void,
-
+    fetchUser: () => void,
+    clear: () => void,
 };
 
-export const useUser = create<UseUser | any>(
+
+export const useStore = create((set) => ({
+  count: 1,
+  inc: () => set((state: any) => ({ count: state.count + 1 })),
+}))
+
+
+export const useUserStore = create<UseUser>()(
     persist(
         (set) => ({
             user: null,
             loading: false,
             error: null,
-            setUser: (user: User) => {
-                set({user});
+            setUser: (user: User) => set((state: UseUser) => (
+                {
+                    user
+                }
+            )),
+            async fetchUser()  {
+                const user: User | null = await getUserByToken()
+                set({user: user});
+            },
+            clear() {
+                set({user: null});
             },
         }),
         {
             name: "user-storage",
             storage: createJSONStorage(() => sessionStorage),
+            version: 1,
         }
     )
 );
