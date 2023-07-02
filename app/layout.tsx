e2552cfc88d9077cb54ff55755d9f44aa282705d
Temirbolat <snake2009003@mail.ru>
2023-07-02 18:48:31 +0600

    Some general layout body sizes fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body  className={inter.className} style={{background: "#f0f2f5"}}>
+      <body  className={inter.className} style={{background: "#f0f2f5", height: "100vh"}}>
         <header>
           <Header />
         </header>
