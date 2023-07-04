8ba6f7b88d20ffcd228e42033fa2369eddf822d2
Temirbolat <snake2009003@mail.ru>
2023-07-04 13:10:21 +0600

    Updated services

---
+++ services/subjects.services.ts
@@ -0,0 +1,50 @@
+import localforage from "localforage";
+
+import { fetcher, PomiseResponse } from "./helpers/fetcher";
+import { HOST, TOKEN_KEY_NAME } from "@/services/index";
+
+export async function fetchClassSubject(id: number, page: number = 1): Promise<PomiseResponse> {
+  const accessToken: string | null = await localforage.getItem("access");
+  if (accessToken) {
+    return fetcher(
+      `${HOST}/api/v1/subjects/class_subjects/${id}?page=${page}`,
+      {
+        headers: {
+          Authorization: `${TOKEN_KEY_NAME} ${accessToken}`
+        }
+      }
+    );
+  }
+  return { isOk: false, response: { data: "Авторизуйтесь заново"} };
+};
+
+export async function fetchClassSubjectTeachers(id: number): Promise<PomiseResponse> {
+  const accessToken: string | null = await localforage.getItem("access");
+  if (accessToken) {
+    return fetcher(
+      `${HOST}/api/v1/subjects/class_subjects/${id}/get_teachers`,
+      {
+        headers: {
+          Authorization: `${TOKEN_KEY_NAME} ${accessToken}`
+        }
+      }
+    );
+  }
+  return { isOk: false, response: { data: "Авторизуйтесь заново"} };
+};
+
+export async function registerForSubject(id: number): Promise<PomiseResponse> {
+  const accessToken: string | null = await localforage.getItem("access");
+  if (accessToken) {
+    return fetcher(
+      `${HOST}/api/v1/subjects/class_subjects/${id}/register`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `${TOKEN_KEY_NAME} ${accessToken}`
+        }
+      }
+    )
+  }
+  return { isOk: false, response: { data: "Авторизуйтесь заново"} };
+}
