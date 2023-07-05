21288fce5b908d3b3f0e4cfdde669b9b5fd6c7ef
Temirbolat <snake2009003@mail.ru>
2023-07-05 18:55:10 +0600

    Updated error handler for messaging.

--- components/Tests/TakeTest/TakeTest.tsx
+++ components/Tests/TakeTest/TakeTest.tsx
@@ -65,7 +65,8 @@ export default function TakeTest() {
           resetStore();
           router.push(`/main/tests/${id}`);
         } else {
-
+          setIsError(true);
+          setInformMsg(resp.response.response);
         }
       })
     };
